577f1b21143f9d61e851ec205713b7a3451cfa44
Marcos Moritz <moritz@portaltelemedicina.com.br>
2021-11-09 21:21:11 -0300

    Add computed config

--- somoConfig.js
+++ somoConfig.js
@@ -4,3 +4,9 @@ class SomoConfig {
   static captureWidth = 160
   static captureHeight = 96
 }
+
+class ComputedConfig {
+  static nSectors = SomoConfig.captureWidth * SomoConfig.captureHeight
+  static sectorWidth = SomoConfig.width / float(SomoConfig.width)
+  static sectorHeight = SomoConfig.height / float(SomoConfig.height)
+}
